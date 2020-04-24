66f4cb53b6697483703406c2dd1fc00266115930
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2020-04-24 14:24:42 +0200

    0.0.1

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import 'vue-simple-markdown/dist/vue-simple-markdown.css'
 
 // Lazy Load Youtube Video
 
+import 'vue-lazy-youtube-video/dist/style.css'
 import Vue from 'vue'
 import LazyYoutubeVideo from 'vue-lazy-youtube-video'
-import 'vue-lazy-youtube-video/dist/style.css'
 
 Vue.component('LazyYoutubeVideo', LazyYoutubeVideo)
 
