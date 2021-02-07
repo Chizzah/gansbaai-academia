07b2d53beb75e87db63e6fc4c797e30706c28276
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2021-02-07 14:19:38 +0200

    0.0.1

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,7 +1,28 @@
 <template>
-  <section class="h-screen flex flex-col justify-center items-center text-center">
-    <h1 class="font-semibold lg:text-4xl">Sorry, this page is still under construction.</h1>
-    <g-image class="my-4 lg:my-8" src="../../static/svg/sad-emoji.svg" alt="Sad emoji" width="240" />
-    <p class="mx-4 lg:text-2xl">Please feel free to head back to the <g-link class="font-semibold text-blue-500 hover:text-blue-300" to="/">home page</g-link> or <g-link class="font-semibold text-blue-500 hover:text-blue-300" to="/contact">contact us</g-link> with any queries you may have.</p>
+  <section
+    class="flex flex-col items-center justify-center h-screen text-center"
+  >
+    <h1 class="font-semibold text-gray-900 lg:text-4xl">
+      Sorry, this page is still under construction.
+    </h1>
+    <g-image
+      class="my-4 lg:my-8"
+      src="../../static/svg/sad-emoji.svg"
+      alt="Sad emoji"
+      width="240"
+    />
+    <p class="mx-4 lg:text-2xl">
+      Please feel free to head back to the
+      <g-link class="font-semibold text-blue-500 hover:text-blue-300" to="/"
+        >home page</g-link
+      >
+      or
+      <g-link
+        class="font-semibold text-blue-500 hover:text-blue-300"
+        to="/contact"
+        >contact us</g-link
+      >
+      with any queries you may have.
+    </p>
   </section>
 </template>
